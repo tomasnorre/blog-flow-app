6da22c155c3bff314519c1908c21a4da8f2d0761
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2015-05-03 08:37:14 +0200

    Added test for prove of concept

---
+++ Tests/Unit/DefaultTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Randomip\Flowapp\Tests\Unit;
+
+class DefaultTest extends \TYPO3\Flow\Tests\BaseTestCase {
+
+	/**
+	 * @test
+	 */
+	public function defaultTest() {
+		$this->assertTrue(TRUE);
+	}
+
+}
